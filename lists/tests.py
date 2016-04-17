76b4cef0660cb66a8f2eed88f21bcafddb1fd918
Gihun Ham <progh2@gmail.com>
2016-04-18 00:08:05 +0900

    아이템용 모델과 마이그레이션 연계

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,25 @@ from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
+
+class ItemModeTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
 
 # Create your tests here.
 class SmokeTest(TestCase):
@@ -36,3 +55,4 @@ class SmokeTest(TestCase):
 
 #    def test_bad_maths(self):
 #        self.assertEquals(1 +1, 4)
+
