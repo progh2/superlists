0f459693d02c7780e4416d922835324b539a9e8b
Gihun Ham <progh2@gmail.com>
2016-04-17 18:58:46 +0900

    2번째 아이템 입력 테스트

--- lists/tests.py
+++ lists/tests.py
@@ -3,16 +3,35 @@ from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+
 # Create your tests here.
 class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
+
         self.assertEquals(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+
         expected_html = render_to_string('home.html')
+
+        self.assertEquals(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method='POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text' : '신규 작업 아이템'}
+        )
+
         self.assertEquals(response.content.decode(), expected_html)
 
 #    def test_bad_maths(self):
