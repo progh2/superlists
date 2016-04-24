ed5e6ac5ffe7fb5882dc7b1a4ba4069b33723e9a
Gihun Ham <progh2@gmail.com>
2016-04-24 18:47:45 +0900

    #1 functional_tests 앱 생성 및 LiveServerTestCase 사용'

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -18,11 +18,11 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹사이트를 확인하러 간다.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url )
 
         # 웹 페이지 타이틀과 헤더가 "To-Do"를 표시하고 있다.
         self.assertIn('To-Do', self.browser.title)
-        #self.fail('Finish the test!')
+        # self.fail('Finish the test!')
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
@@ -40,10 +40,9 @@ class NewVisitorTest(unittest.TestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다
         inputbox.send_keys(Keys.ENTER)
-        #import time
-        #time.sleep(10)
+        # import time
+        # time.sleep(10)
         self.check_for_row_in_list_table('1: 공작깃털 사기')
-
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다(에디스는 매우 체계적인 사람이다)
@@ -63,6 +62,3 @@ class NewVisitorTest(unittest.TestCase):
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
 
         # 만족하고 잠자리에 든다
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
