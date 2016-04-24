0dc6d20822bf2de07f666794a1654b5d38bbe5e5
Gihun Ham <progh2@gmail.com>
2016-04-24 18:18:00 +0900

    POST 후에 리다이렉션과 템플릿에 모든 작업 아이템 표시

--- lists/tests.py
+++ lists/tests.py
@@ -41,18 +41,40 @@ class SmokeTest(TestCase):
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method='POST'
-        request.POST['item_text'] = '신규 작업 아이템'
 
+        request.POST['item_text'] = '신규 작업 아이템'
         response = home_page(request)
 
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text' : '신규 작업 아이템'}
-        )
+        self.assertEquals(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        # 외와 동일한 내용 new_item = Item.objects.all()[0]
+        self.assertEquals(new_item.text, '신규 작업 아이템')
 
-        self.assertEquals(response.content.decode(), expected_html)
 
+    def test_home_page_redirects_after_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+
+        request.POST['item_text'] = '신규 작업 아이템'
+        response = home_page(request)
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response['location'], '/')
+
+    def test_홈페이지는_필요할때만_아이템을_저장해야한다(self): #home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEquals(Item.objects.count(), 0)
+
+
+    def test_home_page_displays_all_list_itmes(self):
+        Item.objects.create(text='함기훈 하나')
+        Item.objects.create(text='조성미 둘')
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('함기훈 하나', response.content.decode())
+        self.assertIn('조성미 둘', response.content.decode())
 #    def test_bad_maths(self):
 #        self.assertEquals(1 +1, 4)
 
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from lists.models import Item
+
 # Create your views here.
 #def home_page():
 #    pass
 
+
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    # TODO: 모든 요청에 대해 비어있는 요청을 저장하지 않는다
+    # TODO: 테이블에 아이템 여러 개 표시하기
+    # TODO: 하나 이상의 목록 지원하기
+
+    if request.method == 'POST':
+        Item.objects.create(text = request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items':items})
